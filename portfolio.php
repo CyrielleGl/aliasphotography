55d2a6d5c354c8fd46728fdfc6d2032a6bb6cf7e
CyrielleGl <cyrielle.gallou@gmail.com>
2020-05-15 01:09:57 +0200

    portfolio

--- portfolio.php
+++ portfolio.php
@@ -8,30 +8,31 @@
         <div class="diapo"></div>
     </div>
 
-    <table>
-        <tr>
-            <td>
-                <img src="./portfolio/melanie-1.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
-                <img src="./portfolio/juliette-1.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
-                <img src="./portfolio/darkprincess-1.jpg" id="photo_home" class="responsive_portfolio" title="Alternatif Magie bleue Hiver Bois de Vincennes" alt="Alternatif Magie bleue Hiver Bois de Vincennes"/>
-            </td>
+        <table>
+            <tr>
+                <td>
+                    <img src="./portfolio/melanie-1.jpg" class="responsive_portfolio" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
+                    <img src="./portfolio/juliette-1.jpg" class="responsive_portfolio" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
+                    <img src="./portfolio/darkprincess-1.jpg" class="responsive_portfolio" title="Alternatif Magie bleue Hiver Bois de Vincennes" alt="Alternatif Magie bleue Hiver Bois de Vincennes"/>
+                </td>
+                
+                <td>
+                    <img src="./portfolio/darkprincess-2.jpg" class="responsive_portfolio" title="Alternatif Magie bleue Hiver Bois de Vincennes" alt="Alternatif Magie bleue Hiver Bois de Vincennes"/>
+                    <img src="./portfolio/pauline-3.jpg" class="responsive_portfolio" title="Portrait Femme Paris Louvre" alt="Portrait Femme Paris Louvre"/>
+                </td>
+                
+                <td>
+                    <img src="./portfolio/pauline-1.jpg" class="responsive_portfolio" title="Portrait Femme Paris Louvre" alt="Portrait Femme Paris Louvre"/>
+                    <img src="./portfolio/juliette-2.jpg" class="responsive_portfolio" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
+                    <img src="./portfolio/melanie-2.jpg" class="responsive_portfolio" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
+                    <img src="./portfolio/juliette-4.jpg" class="responsive_portfolio" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
+                </td>
             
-            <td>
-                <img src="./portfolio/darkprincess-2.jpg" id="photo_home" class="responsive_portfolio" title="Alternatif Magie bleue Hiver Bois de Vincennes" alt="Alternatif Magie bleue Hiver Bois de Vincennes"/>
-                <img src="./portfolio/pauline-3.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme Paris Louvre" alt="Portrait Femme Paris Louvre"/>
-            </td>
-            
-            <td>
-                <img src="./portfolio/pauline-1.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme Paris Louvre" alt="Portrait Femme Paris Louvre"/>
-                <img src="./portfolio/juliette-2.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
-                <img src="./portfolio/melanie-2.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
-                <img src="./portfolio/juliette-4.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
-            </td>
-        
-        </tr>
-    </table>
-
+            </tr>
+        </table>
 </div>
+
+
 
 <div class="portfolio_mobile">
                 <img src="./portfolio/melanie-1.jpg" id="photo_home" class="responsive" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
