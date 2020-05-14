e592dca6337a76d3fe60748a471e502475c14eae
CyrielleGl <cyrielle.gallou@gmail.com>
2020-05-15 00:23:57 +0200

    responsive

--- portfolio.php
+++ portfolio.php
@@ -11,21 +11,21 @@
     <table>
         <tr>
             <td>
-                <img src="./portfolio/melanie-1.jpg" id="photo_home" class="responsive" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
-                <img src="./portfolio/juliette-1.jpg" id="photo_home" class="responsive" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
-                <img src="./portfolio/darkprincess-1.jpg" id="photo_home" class="responsive" title="Alternatif Magie bleue Hiver Bois de Vincennes" alt="Alternatif Magie bleue Hiver Bois de Vincennes"/>
+                <img src="./portfolio/melanie-1.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
+                <img src="./portfolio/juliette-1.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
+                <img src="./portfolio/darkprincess-1.jpg" id="photo_home" class="responsive_portfolio" title="Alternatif Magie bleue Hiver Bois de Vincennes" alt="Alternatif Magie bleue Hiver Bois de Vincennes"/>
             </td>
             
             <td>
-                <img src="./portfolio/darkprincess-2.jpg" id="photo_home" class="responsive" title="Alternatif Magie bleue Hiver Bois de Vincennes" alt="Alternatif Magie bleue Hiver Bois de Vincennes"/>
-                <img src="./portfolio/pauline-3.jpg" id="photo_home" class="responsive" title="Portrait Femme Paris Louvre" alt="Portrait Femme Paris Louvre"/>
+                <img src="./portfolio/darkprincess-2.jpg" id="photo_home" class="responsive_portfolio" title="Alternatif Magie bleue Hiver Bois de Vincennes" alt="Alternatif Magie bleue Hiver Bois de Vincennes"/>
+                <img src="./portfolio/pauline-3.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme Paris Louvre" alt="Portrait Femme Paris Louvre"/>
             </td>
             
             <td>
-                <img src="./portfolio/pauline-1.jpg" id="photo_home" class="responsive" title="Portrait Femme Paris Louvre" alt="Portrait Femme Paris Louvre"/>
-                <img src="./portfolio/juliette-2.jpg" id="photo_home" class="responsive" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
-                <img src="./portfolio/melanie-2.jpg" id="photo_home" class="responsive" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
-                <img src="./portfolio/juliette-4.jpg" id="photo_home" class="responsive" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
+                <img src="./portfolio/pauline-1.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme Paris Louvre" alt="Portrait Femme Paris Louvre"/>
+                <img src="./portfolio/juliette-2.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
+                <img src="./portfolio/melanie-2.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
+                <img src="./portfolio/juliette-4.jpg" id="photo_home" class="responsive_portfolio" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
             </td>
         
         </tr>
