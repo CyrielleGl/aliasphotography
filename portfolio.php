dd7ae02e3b73ec6af2d352651f8f9806819e493f
CyrielleGl <cyrielle.gallou@gmail.com>
2020-05-20 15:49:28 +0200

    photos mobile

--- portfolio.php
+++ portfolio.php
@@ -38,12 +38,15 @@
 
 
 <div class="portfolio_mobile">
+                <img src="./portfolio/claire-1.jpg" class="responsive_portfolio" title="Shooting Elfe Blanc Hiver" alt="Shooting Elfe Blanc Hiver"/>
                 <img src="./portfolio/melanie-1.jpg" id="photo_home" class="responsive" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
                 <img src="./portfolio/juliette-1.jpg" id="photo_home" class="responsive" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
                 <img src="./portfolio/darkprincess-2.jpg" id="photo_home" class="responsive" title="Alternatif Magie bleue Hiver Bois de Vincennes" alt="Alternatif Magie bleue Hiver Bois de Vincennes"/>
                 <img src="./portfolio/pauline-3.jpg" id="photo_home" class="responsive" title="Portrait Femme Paris Louvre" alt="Portrait Femme Paris Louvre"/>
+                <img src="./portfolio/claire-2.jpg" class="responsive_portfolio" title="Shooting Elfe Blanc Hiver" alt="Shooting Elfe Blanc Hiver"/>
                 <img src="./portfolio/melanie-2.jpg" id="photo_home" class="responsive" title="Portrait Femme asiatique BNF Paris Hiver" alt="Portrait Femme asiatique BNF Paris Hiver"/>
                 <img src="./portfolio/pauline-1.jpg" id="photo_home" class="responsive" title="Portrait Femme Paris Louvre" alt="Portrait Femme Paris Louvre"/>
+                <img src="./portfolio/claire-3.jpg" class="responsive_portfolio" title="Shooting Elfe Blanc Hiver" alt="Shooting Elfe Blanc Hiver"/>
                 <img src="./portfolio/juliette-2.jpg" id="photo_home" class="responsive" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/> 
                 <img src="./portfolio/darkprincess-1.jpg" id="photo_home" class="responsive" title="Alternatif Magie bleue Hiver Bois de Vincennes" alt="Alternatif Magie bleue Hiver Bois de Vincennes"/>
                 <img src="./portfolio/juliette-4.jpg" id="photo_home" class="responsive" title="Portrait Femme Jardin Paris" alt="Portrait Femme Jardin Paris"/>
